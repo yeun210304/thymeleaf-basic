01dd4adbaa3aea7abf807a63253404df95c04a7c
Yeun <80025908+yeun210304@users.noreply.github.com>
2025-03-08 12:09:44 +0900

    SpringEL 표현식

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,9 +1,16 @@
 package hello.thymeleaf.basic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.Data;
 
 @Controller
 @RequestMapping("/basic")
@@ -21,4 +28,34 @@ public class BasicController {
         return "basic/text-unescaped";
     }
 
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        List<User> lst = new ArrayList<>();
+        lst.add(userA);
+        lst.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", lst);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+    @Data
+    static class User {
+        private String username;
+        private int age;
+
+        private User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
+    }
 }
