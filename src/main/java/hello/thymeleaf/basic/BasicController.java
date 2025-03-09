bd48d6ac501b3c03d2a15daa2a3cb6bc730d72e7
Yeun <80025908+yeun210304@users.noreply.github.com>
2025-03-09 12:28:24 +0900

    블록

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -113,6 +113,12 @@ public class BasicController {
         return "basic/comments";
     }
 
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
     private void addUsers(Model model) {
         List<User> lst = new ArrayList<>();
         lst.add(new User("UserA", 10));
